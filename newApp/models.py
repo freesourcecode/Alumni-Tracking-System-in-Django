be54c7ae75b68aea41deb1f413dc05ce4d5407ef
Joken <Joken@GAMDIAS.com>
2025-07-29 16:26:18 +0800

    initial commit

---
+++ newApp/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from PIL import Image
+from django.contrib.auth.models import User
+
+COLLEGE_CHOICES = (
+    ('Carlos Hilado Memorial State College, Binalbagan',
+     'Carlos Hilado Memorial State College, Binalbagan'),
+    ('Carlos Hilado Memorial State College, Talisay',
+     'Carlos Hilado Memorial State College, Talisay'),
+    ('Carlos Hilado Memorial State College, Fortune Town',
+     'Carlos Hilado Memorial State College, Fortune Town'),
+    ('Carlos Hilado Memorial State College, Alijis',
+     'Carlos Hilado Memorial State College, Alijis'),
+)
+
+
+class User(AbstractUser):
+    #    is_alumni = models.BooleanField(default=False)
+    is_college = models.BooleanField(default=False)
+    College = models.CharField(
+        max_length=80,
+        choices=COLLEGE_CHOICES,
+        default="None"
+    )
+    About = models.TextField(max_length=800)
+    Work = models.TextField(max_length=50)
+    Year_Joined = models.CharField(max_length=4)
+    Branch = models.CharField(max_length=50)
+    Image = models.ImageField(
+        upload_to='images',
+        default='default/test.png'
+    )
+    Verified = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.Image.path)
+        if img.height > 500 or img.width > 500:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.Image.path)
